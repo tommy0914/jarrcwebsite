fb02bd803cc80f3616e5b5d484d22f644a22e90b
youngemeritus <sobalajetomiwa@gmail.com>
2026-03-21 06:01:59 -0700

    Replace search icon with hamburger menu icon

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,8 +14,8 @@
 
     <div class="flex items-center gap-4">
       <div class="w-10 h-10 border border-gray-300 rounded-full flex items-center justify-center cursor-pointer hover:bg-white transition-all shadow-sm">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
       </div>
     </div>
